47efa98224327debae52f1c89e6f413b6f22c7af
sinasi cicek <sinasi.cicek@ogr.iu.edu.tr>
2022-12-21 23:20:48 +0300

    weather app responsive olarak hazır

---
+++ weather_app/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import hava from "./components/weather-state.vue";
+export default {
+  components:{hava}
+}
+</script>
+
+<template>
+  <div>
+    <hava></hava>
+  </div>
+</template>
+
+<style>
+</style>
+
